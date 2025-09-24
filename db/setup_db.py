0162592de865fad7cb0b42ec0670641ab86f5759
Nishant Sethunath <36416402+nishs9@users.noreply.github.com>
2025-09-23 20:04:36 -0700

    updated db script

--- db/setup_db.py
+++ db/setup_db.py
@@ -16,9 +16,36 @@ def generate_derived_column_stats(df, year):
     df['receiving_adot'] = df['receiving_air_yards'] / df['targets']
     return df
 
-def create_database():
-    # Create database connection
-    conn = sqlite3.connect('nfl_stats.db')
+def get_player_stats_data():
+    player_stats_data_df_list = {}
+    for year in range(1999, 2026):
+        print(f"Downloading player stats for {year}")
+        base_url_std = 'https://github.com/nflverse/nflverse-data/releases/download/player_stats/player_stats_season_' + str(year) + '.csv.gz'
+        if year == 2025:
+            base_url_std = 'https://github.com/nflverse/nflverse-data/releases/download/stats_player/stats_player_regpost_' + str(year) + '.csv.gz'
+        raw_player_stats_data = pd.read_csv(base_url_std, compression='gzip', low_memory=False)
+        player_stats_data_df_list[year] = raw_player_stats_data
+    return player_stats_data_df_list
+
+def create_database_online(conn: sqlite3.Connection):
+    # Establish db connection 
+    cursor = conn.cursor()
+
+    player_stats_data_df_list = get_player_stats_data()
+    for year, df in player_stats_data_df_list.items():
+        table_name = f'player_stats_season_{year}'
+        df = generate_derived_column_stats(df, year)
+        df.to_sql(table_name, conn, if_exists='replace', index=False)
+        cursor.execute(f"CREATE INDEX IF NOT EXISTS idx_{table_name}_player_season ON {table_name}(player_id, season)")
+        print(f"Created table {table_name} with {len(df)} rows")
+
+    # Commit changes and close connection
+    conn.commit()
+    conn.close()
+    print("Online database setup complete!")
+
+def create_database(conn: sqlite3.Connection):
+    # Establish database connection
     cursor = conn.cursor()
 
     # Get all CSV files in the player_stats_season directory
@@ -52,4 +79,5 @@ def create_database():
     print("Database setup complete!")
 
 if __name__ == "__main__":
-    create_database() 
+    conn = sqlite3.connect('nfl_stats.db')
+    create_database_online(conn) 
